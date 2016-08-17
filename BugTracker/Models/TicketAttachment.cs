2d9657441226105b77ed0242054fa46a6f28c6ab
annette-arrigucci <annette.arrigucci@gmail.com>
2016-08-17 13:37:19 -0400

    preparing to add comments and attachments interface

--- BugTracker/Models/TicketAttachment.cs
+++ BugTracker/Models/TicketAttachment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,7 @@ namespace BugTracker.Models
         public int Id { get; set; }
         public int TicketId { get; set; }
         public string FilePath { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
         public DateTimeOffset Created { get; set; }
         public string UserId { get; set; }
