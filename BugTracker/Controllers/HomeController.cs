7e5d84f04691f3d62f333246f9c0eaccf09e272c
annette-arrigucci <annette.arrigucci@gmail.com>
2016-08-29 11:50:14 -0400

    final styling fixes, new About page

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -15,12 +15,10 @@ namespace BugTracker.Controllers
             //return View();
         }
 
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
+        public ActionResult About()
+        {
+            return View();
+        }
 
     }
 }
